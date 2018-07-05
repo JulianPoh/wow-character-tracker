7d9df9899d3cc0cfb30151874a425b887a3e63f0
JulianPoh <julianpoh01@gmail.com>
2018-07-05 16:30:06 +0800

    fixed character race selection

--- public/script.js
+++ public/script.js
@@ -1,10 +1,8 @@
 
-const allianceRaces = ["Human", "Dwarf", "Night Elf", "Gnome", "Draenei", "Worgen", "Pandarian"];
-const hordeRaces = ["Orc", "Undead", "Blood Elf", "Goblin", "Troll", "Tauren", "Pandarian"];
+const allianceRaces = ["Select Race", "Human", "Dwarf", "Night Elf", "Gnome", "Draenei", "Worgen", "Pandarian"];
+const hordeRaces = ["Select Race", "Orc", "Undead", "Blood Elf", "Goblin", "Troll", "Tauren", "Pandarian"];
 
 console.log('Script Loaded');
-console.log(allianceRaces);
-console.log(hordeRaces);
 
 const factionSelection = document.getElementById("select-faction");
 const raceSelection = document.getElementById("select-race");
@@ -13,24 +11,20 @@ const classSelection = document.getElementById("select-class");
     factionSelection.addEventListener('change', function() {
 
         if (factionSelection.value == "alliance") {
-
-            allianceRaceOptions = document.createElement("option");
-
+            raceSelection.options.length = 0;
             for (var i=0; i<allianceRaces.length; i++) {
-                console.log(allianceRaceOptions);
+                var allianceRaceOptions = document.createElement("option");
                 allianceRaceOptions.value = allianceRaces[i];
                 allianceRaceOptions.text = allianceRaces[i];
                 raceSelection.appendChild(allianceRaceOptions);
             }
             
         } else if (factionSelection.value == "horde") {
-
-            hordeRaceOptions = document.createElement("option");
-
-            for (var j=0; j<hordeRaces.length; j++) {
-                console.log(j);
-                hordeRaceOptions.value = hordeRaces[j];
-                hordeRaceOptions.text = hordeRaces[j];
+            raceSelection.options.length = 0;
+            for (var i=0; i<hordeRaces.length; i++) {
+                var hordeRaceOptions = document.createElement("option");
+                hordeRaceOptions.value = hordeRaces[i];
+                hordeRaceOptions.text = hordeRaces[i];
                 raceSelection.appendChild(hordeRaceOptions);
         }
     }
